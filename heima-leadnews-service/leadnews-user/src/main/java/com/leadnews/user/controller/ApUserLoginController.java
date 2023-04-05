b427db4680cc5d9f1beef0d282d7dc30633ce778
elianbeyond <814846181@qq.com>
2023-04-05 21:24:03 +1000

    swagger

--- heima-leadnews-service/leadnews-user/src/main/java/com/leadnews/user/controller/ApUserLoginController.java
+++ heima-leadnews-service/leadnews-user/src/main/java/com/leadnews/user/controller/ApUserLoginController.java
@@ -3,6 +3,8 @@ package com.leadnews.user.controller;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.user.dtos.LoginDto;
 import com.leadnews.user.service.impl.ApUserServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/login")
+@Api(value = "app端用户登录",tags = "app端用户登录", description = "app端用户登录API")
 public class ApUserLoginController {
     @Autowired
     private ApUserServiceImpl apUserService;
     @PostMapping("/login_auth")
+    @ApiOperation("用户登录")
     public ResponseResult login(@RequestBody LoginDto dto){
         return apUserService.login(dto);
     }
--- heima-leadnews-service/leadnews-user/src/main/java/com/leadnews/user/service/impl/ApUserServiceImpl.java
+++ heima-leadnews-service/leadnews-user/src/main/java/com/leadnews/user/service/impl/ApUserServiceImpl.java
@@ -1,28 +1,67 @@
 package com.leadnews.user.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
 import com.heima.model.user.dtos.LoginDto;
 import com.heima.model.user.pojos.ApUser;
+import com.heima.utils.common.AppJwtUtil;
 import com.leadnews.user.mapper.ApUserMapper;
 import com.leadnews.user.service.ApUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.checkerframework.checker.units.qual.A;
 import org.jsoup.helper.StringUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Transactional
 @Slf4j
-public class ApUserServiceImpl extends ServiceImpl<ApUserMapper,ApUser> implements ApUserService {
+public class ApUserServiceImpl extends ServiceImpl<ApUserMapper, ApUser> implements ApUserService {
     @Override
     public ResponseResult login(LoginDto dto) {
-        if(!StringUtils.isNotBlank(dto.getPhone())&&!StringUtils.isNotBlank(dto.getPassword())){
-            return null;
+        if (!StringUtils.isNotBlank(dto.getPhone()) && !StringUtils.isNotBlank(dto.getPassword())) {
+
+            //根据手机号查数据库中的信息
+            //常规写法
+//        QueryWrapper<ApUser> apUserQueryWrapper = new QueryWrapper<>();
+//        apUserQueryWrapper.eq("phone",dto.getPhone());
+//        ApUser apUser = new ApUser();
+            //apUser = getOne(apUserQueryWrapper);
+            ApUser apUser = getOne(Wrappers.<ApUser>lambdaQuery().eq(ApUser::getPhone, dto.getPhone()));
+
+            if (apUser == null) {
+                return ResponseResult.errorResult(AppHttpCodeEnum.DATA_NOT_EXIST, "用户不存在");
+            }
+
+
+            //1.2 比对密码
+            String salt = apUser.getSalt();
+            String pswd = dto.getPassword();
+            pswd = DigestUtils.md5DigestAsHex((pswd + salt).getBytes());
+            if (!pswd.equals(apUser.getPassword())) {
+                return ResponseResult.errorResult(AppHttpCodeEnum.LOGIN_PASSWORD_ERROR);
+            }
+            //1.3 返回数据  jwt
+            Map<String, Object> map = new HashMap<>();
+            map.put("token", AppJwtUtil.getToken(apUser.getId().longValue()));
+            apUser.setSalt("");
+            apUser.setPassword("");
+            map.put("user", apUser);
+            return ResponseResult.okResult(map);
+        } else {
+            //2.游客  同样返回token  id = 0
+            Map<String, Object> map = new HashMap<>();
+            map.put("token", AppJwtUtil.getToken(0l));
+            return ResponseResult.okResult(map);
         }
-        return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
 
 
     }
